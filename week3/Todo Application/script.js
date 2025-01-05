025c0131b3e41874d01fc716892b7675e0cd982b
AayushChitransh11 <aayush11.ac@gmail.com>
2025-01-04 23:08:39 -0500

    new code

--- week3/Todo Application/script.js
+++ week3/Todo Application/script.js
@@ -1,16 +1,41 @@
- let counter=1;
- function addTODO(){
-    const inputElement=document.getElementById("input");
-    const newElement=document.createElement("div");
-    newElement.setAttribute("id",counter);
+//  let counter=1;
+//  function addTODO(){
+//     const inputElement=document.getElementById("input");
+//     const newElement=document.createElement("div");
+//     newElement.setAttribute("id",counter);
     
-    newElement.innerHTML="<div>" +counter+". "+inputElement.value+"</div> <button onclick='deleteTODO("+counter+")'>Delete</button>";
-    const parentElement=inputElement.parentNode;
-    parentElement.appendChild(newElement);
-    counter+=1;
+//     newElement.innerHTML="<div>" +counter+". "+inputElement.value+"</div> <button onclick='deleteTODO("+counter+")'>Delete</button>";
+//     const parentElement=inputElement.parentNode;
+//     parentElement.appendChild(newElement);
+//     counter+=1;
+// }
+
+// function deleteTODO(index){
+//     const deleteElement=document.getElementById(index);
+//     deleteElement.parentNode.removeChild(deleteElement);
+// }
+//another way
+let ctr=1;
+
+function addTodo(){
+  let inputElement=document.getElementById("input");
+  let newElement=document.createElement("div");
+  newElement.setAttribute("id",ctr);
+  // newElement.innerHTML="<div>"+ctr+". "+inputElement.value+"</div><button onclick='deleteTodo("+ctr+")'>Delete</button>";
+  let spanElement=document.createElement("span");
+  let btnElement=document.createElement("button");
+  spanElement.innerHTML=ctr+". "+inputElement.value;
+  btnElement.setAttribute("onclick","deleteTodo("+ctr+")");
+  btnElement.innerHTML="Delete";
+  newElement.appendChild(spanElement);
+  newElement.appendChild(btnElement);
+  
+  let parentElement=inputElement.parentNode;
+  parentElement.appendChild(newElement);
+  ctr+=1;
 }
 
-function deleteTODO(index){
-    const deleteElement=document.getElementById(index);
-    deleteElement.parentNode.removeChild(deleteElement);
+function deleteTodo(index){
+  let deleteElement=document.getElementById(index);
+  deleteElement.parentNode.removeChild(deleteElement);
 }
